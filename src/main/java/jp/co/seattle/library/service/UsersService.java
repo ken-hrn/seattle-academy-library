82d7e2518380f24dd6143347a782571bfe4c555b
ken-hrn <kentaro.hirano@seattleconsulting.co.jp>
2022-04-18 15:18:24 +0900

    create display author and publisher in home.jsp

--- src/main/java/jp/co/seattle/library/service/UsersService.java
+++ src/main/java/jp/co/seattle/library/service/UsersService.java
@@ -3,6 +3,7 @@ package jp.co.seattle.library.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 
